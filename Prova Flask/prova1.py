2335698d91a3bf390f6762d65b29317b55c4a8a0
MarianoCaccavale <75137536+MarianoCaccavale@users.noreply.github.com>
2021-02-18 09:42:04 +0100

    Update prova1.py

--- Prova Flask/prova1.py
+++ Prova Flask/prova1.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("template1.html", content="prova contenuto dinamica riuscita")
 
-@app.route("/<name><cognome>")
-def user(name, cognome):
-    return f"Hello {name} {cognome}!"
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
 
 @app.route("/admin")
 def admin():
-	return redirect(url_for("user", name= "Admin", cognome = "di questo sito"))
+	return redirect(url_for("user", name= "Admin"))
 
 if __name__ == "__main__":
     app.run()
