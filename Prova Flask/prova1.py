d1ce30fac60f0d378cc65cb37e8167886b015d23
MarianoCaccavale <75137536+MarianoCaccavale@users.noreply.github.com>
2021-02-21 10:58:45 +0100

    update

--- Prova Flask/prova1.py
+++ Prova Flask/prova1.py
@@ -1,18 +1,31 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['POST', 'GET'])
 def home():
     return render_template("template1.html", content="prova contenuto dinamica riuscita")
-
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
 
 @app.route("/admin")
 def admin():
 	return redirect(url_for("user", name= "Admin"))
 
+@app.route("/culo")
+def culo():
+    return f"<h1>CULO</h1>"
+
+
+
+@app.route("/login", methods = ['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+            
+            return render_template("login.html")
+    else:
+            return redirect( url_for ('culo'))
+
+
+app.static_folder = 'static'
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
